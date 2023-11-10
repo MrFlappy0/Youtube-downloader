94891088ab82756721c28a135cbdb45aafd198a5
Theo Noel <135755037+MrFlappy0@users.noreply.github.com>
2023-11-10 22:34:10 +0100

    setup for install

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import os
+import shutil
+import time
+import urllib.request
 
 # Installer les modules nécessaires
-os.system("pip install pytube tqdm wget")
+os.system("pip install pytube tqdm ")
 
 # Créer le dossier de téléchargement et le fichier url.txt
 download_dir = os.path.join(os.path.expanduser("~"), "Downloads", "Youtube download")
@@ -15,4 +18,19 @@ else:
         os.mkdir(os.path.join(download_dir, "data"))
 
 # Télécharger le fichier Python à partir de GitHub
-os.system("wget https://github.com/MrFlappy0/Youtube-downloader/releases/download/Base/download.py" + download_dir)
+url = "https://raw.githubusercontent.com/MrFlappy0/Youtube-downloader/main/download.py"
+filename = os.path.join(download_dir, "download.py")
+urllib.request.urlretrieve(url, filename)
+
+# Vérifier que tous les fichiers sont bien présents
+if os.path.exists(os.path.join(download_dir, "download.py")) and os.path.exists(os.path.join(download_dir, "url.txt")):
+    print("Tous les fichiers ont été installés dans la rubrique téléchargement de l'explorateur.")
+    time.sleep(5)
+    # Demander à l'utilisateur s'il souhaite désinstaller le fichier setup.py
+    response = input("Souhaitez-vous désinstaller le fichier setup.py ? (y/n) ")
+    if response.lower() == "y":
+        os.remove(os.path.join(os.getcwd(), "setup.py"))
+    else:
+        exit()
+else:
+    print("Erreur : tous les fichiers n'ont pas été installés correctement.")
